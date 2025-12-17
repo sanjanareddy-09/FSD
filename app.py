43803760c6f56de8d98f33c3cab14468d009565d
sanjanareddy-09 <sanjanareddyrokkam@gmail.com>
2025-12-17 12:15:11 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ st.set_page_config (
     page_title="Student Task Dashboard",
     layout="wide"
 )
-html_file=Path("index.html").read_text(encoding="utf-8")
+html_file=Path("project.html").read_text(encoding="utf-8")
 st.components.v1.html(html_file,height=900,scrolling=True)
